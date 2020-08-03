b209ffee5742b7d32db7c92e0f0ac8cf3ba36106
Fabricio Duarte <fabriciohod@hotmail.com>
2020-08-03 11:49:35 -0300

    Implement router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Landing from './pages/Landing';
+import Routes from './Routes';
 import './assets/styles/global.css';
 
 
@@ -7,7 +7,7 @@ function App ()
 {
   return (
     <div className="App">
-      <Landing />
+      <Routes />
     </div>
   );
 }
--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
@@ -24,15 +25,15 @@ function index ()
                 />
 
                 <div className="buttons-container">
-                    <a href="" className="study">
+                    <Link to="/study" className="study">
                         <img src={studyIcon} alt="Estudar" />
                         Estudar
-                    </a>
+                    </Link>
 
-                    <a href="" className="give-classes">
+                    <Link to="/give-classes" className="give-classes">
                         <img src={giveClassIcon} alt="Dar aulas" />
                         Dar aulas
-                    </a>
+                    </Link>
                 </div>
 
                 <span className="total-connections">
