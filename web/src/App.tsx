fd536fb4eb220c3b0cd2da8d5ed66e329ac6aaa0
Fabricio Duarte <fabriciohod@hotmail.com>
2020-08-03 11:01:48 -0300

    Add Lnading page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Landing from './pages/Landing';
 import './assets/styles/global.css';
 
 
@@ -6,7 +7,7 @@ function App ()
 {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Landing />
     </div>
   );
 }
