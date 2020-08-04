f16ef25b02471a99556ff76a691a53ae020676a6
Fabricio Duarte <fabriciohod@hotmail.com>
2020-08-04 14:36:05 -0300

    feat: Add count for connections and install Cors

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,13 +1,14 @@
 import express, { response } from 'express';
-import ClassesControllers from './controllers/ClassesController';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
-const classesControllers = new ClassesControllers;
+const classesController = new ClassesController;
+const connectionsController = new ConnectionsController;
 
-
-routes.post('/classes', classesControllers.create);
-routes.get('/classes', classesControllers.index);
-
-
+routes.post('/classes', classesController.create);
+routes.get('/classes', classesController.index);
+routes.post('/connections', connectionsController.create);
+routes.get('/connections', connectionsController.index);
 
 export default routes;
