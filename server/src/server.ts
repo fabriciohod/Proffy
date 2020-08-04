7416fa1da77c27a669d38cd7620e6c11a526c4d0
Fabricio Duarte <fabriciohod@hotmail.com>
2020-08-04 12:49:47 -0300

    feat: Create user and schedules

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,10 @@
 import express from 'express';
+import routes from './routes';
+
 const app = express();
 
 app.use(express.json());
 
-app.post('/users', (req, res) =>
-{
-    console.log(req.body);
-    const users = [
-        { nome: 'fabricio', idade: 20 }
-    ];
-    return res.json(users);
-});
+app.use(routes)
 
 app.listen(3333);
