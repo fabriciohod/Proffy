fcd6b31a120c54a353520eee0204ca98fcd34f00
Fabricio Duarte <fabriciohod@hotmail.com>
2020-08-04 10:03:54 -0300

    Building dataBase sqlite3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
-
 const app = express();
 
-app.get('/users', (req, res) =>
+app.use(express.json());
+
+app.post('/users', (req, res) =>
 {
-    res.send('hello world');
+    console.log(req.body);
+    const users = [
+        { nome: 'fabricio', idade: 20 }
+    ];
+    return res.json(users);
 });
 
 app.listen(3333);
