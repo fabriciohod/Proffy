babe6c78ffb077c6d4a0cd25c80c7bdec172d0c2
Fabricio Duarte <fabriciohod@hotmail.com>
2020-08-04 13:02:34 -0300

    Create ClassesController

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,66 +1,11 @@
 import express, { response } from 'express';
-import db from './database/connection';
-import covertHourToMinutes from './utils/covertHourToMinutes';
+import ClassesControllers from './controllers/ClassesController';
+
 const routes = express.Router();
+const classesControllers = new ClassesControllers;
 
-interface ScheduleItem
-{
-    week_day: number,
-    from: string,
-    to: string;
-}
 
-routes.post('/classes', async (req, res) =>
-{
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    } = req.body;
-    const trx = await db.transaction();
-
-    try {
-        const insertedUserIds = await trx('users').insert({
-            name,
-            avatar,
-            whatsapp,
-            bio,
-        });
-
-        const user_id = insertedUserIds[0];
-        const insertedClassesIds = await trx('classes').insert({
-            subject,
-            cost,
-            user_id,
-        });
-
-        const class_id = insertedClassesIds[0];
-        const classSchedule = schedule.map((scheduleItem: ScheduleItem) =>
-        {
-            return {
-                class_id,
-                week_day: scheduleItem.week_day,
-                from: covertHourToMinutes(scheduleItem.from),
-                to: covertHourToMinutes(scheduleItem.to)
-            };
-        });
-
-        await trx('class_schedule').insert(classSchedule);
-
-        await trx.commit();
-        return res.status(201).send();
-    } catch (error) {
-        trx.rollback();
-
-        return res.status(400).json({
-            error
-        });
-    }
-});
+routes.post('/classes', classesControllers.create);
 
 
 
