81dfd0bb49e9c62879f9cb00861c4e55ec16521d
Fabricio Duarte <fabriciohod@hotmail.com>
2020-08-05 10:52:02 -0300

    New Input component and fix a correcting a typo in global.css

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
-import './styles.css';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import './styles.css';
 
 function TeacherList ()
 {
@@ -9,18 +10,9 @@ function TeacherList ()
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject" />
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="week-day">Dia da semana</label>
-                        <input type="text" id="week-day" />
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time" />
-                    </div>
+                    <Input name="subject" label="Matéria" />
+                    <Input name="week_day" label="Dia da semana" />
+                    <Input type="time" name="time" label="Hora" />
                 </form>
             </PageHeader>
             <main>
